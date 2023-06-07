f0e958960b50bc5571632553a1f218f422c58f5e
LeoneCarmelo <carmeloleone@outlook.it>
2023-06-07 16:11:17 +0200

    Add new Model + foreign key to Projects table+seed

---
+++ database/migrations/2023_06_07_135456_add_type_id_foreign_key_to_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            //Add column after id
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+            //Add foreign key
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            //Remove foreign key
+            $table->dropForeign('projects_types_id_foreign');
+            //Drop column
+            $table->dropColumn('type_id');
+
+        });
+    }
+};
